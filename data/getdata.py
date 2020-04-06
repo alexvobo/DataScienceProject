16aa791f7f55d0987152f495546aa91c3e2aee19
Alexsandr Vobornov <vobos2@gmail.com>
2020-04-06 05:48:57 -0400

    Significant performance increases

--- data/getdata.py
+++ data/getdata.py
@@ -2,10 +2,10 @@ import requests
 import glob
 import os
 import pandas as pd
-import pdb
 import csv
 
 dateRange = []
+
 
 # Gets date range and saves it in global dateRange list
 def get_date_range():
@@ -16,37 +16,39 @@ def get_date_range():
         for date in f.readlines():
             dateRange.append(date.rstrip())
 
+
 # Using dates file, requests to download the files we need to data folder.
-def getCSVs():
+def get_csvs():
     b_url = "https://spotifycharts.com/regional/global/weekly/"
-    index = 0
-    with open("dates", "r") as daterange:
-        for d in daterange.readlines():
-            date = d.rstrip()
-            url = b_url + date + "/download"
 
-            r = requests.get(url)
-            with open('data/week' + str(index) + str('.csv'), 'wb') as f:
-                f.write(r.content)
-            index += 1
-        daterange.close()
+    if not dateRange:
+        get_date_range()
+
+    for i, d in enumerate(dateRange):
+        url = b_url + d + "/download"
+
+        r = requests.get(url)
+        with open('data/week1' + str(i) + str('.csv'), 'wb') as f:
+            f.write(r.content)
 
 
-def cleanCSVs():
-    get_date_range()
+def clean_csvs():
+    if not dateRange:
+        get_date_range()
+
     os.chdir('data')
     path = r'.'
     all_files = glob.glob(os.path.join(path, "*.csv"))
 
-    for i in range(len(all_files)):
-        with open(os.path.basename(all_files[i]), 'r', encoding="utf-8") as csvInput:
-            with open("week " + str(i) + ".csv", 'w') as csvOutput:
+    for i, file in enumerate(all_files):
+        with open(os.path.basename(file), 'r', encoding="utf-8") as csvInput:
+            with open("week " + str(i) + ".csv", 'w', encoding="utf-8") as csvOutput:
                 writer = csv.writer(csvOutput, lineterminator='\n')
                 reader = csv.reader(csvInput)
                 all = []
 
                 # Header
-                skip = next(reader)
+                next(reader)
                 row = next(reader)
                 row.append("Week")
                 all.append(row)
@@ -61,36 +63,30 @@ def cleanCSVs():
                     pass
 
 
-def deleteOldCSV():
+def delete_old_csv():
     path = r'C:\Users\Alex\Documents\Data Science\Project\data\data'
     all_files = glob.glob(os.path.join(path, "*.csv"))
     for f in all_files:
         fname = os.path.basename(f)
-        if " " in fname:
-            print(fname)
+        if " " not in fname:
+            print("removing", fname)
             os.remove(path + "\\" + fname)
-        # if " " not in fname:
-        #     print(fname)
-        #     os.remove(path + "\\" + fname)
 
 
-def createBigCSV():
+def create_big_csv():
     os.chdir('data')
     path = os.curdir
     all_files = glob.glob(
         os.path.join(path, "*.csv"))
-    # pdb.set_trace()
     df_from_each_file = ([pd.read_csv(f, encoding="windows-1252") for f in all_files if " " in os.path.basename(f)])
-    print(df_from_each_file)
+    # print(df_from_each_file)
     concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
     concatenated_df.to_csv(os.path.join(os.pardir, "big.csv"), index=False)
 
 
 if __name__ == "__main__":
+    # get_csvs()
+    # clean_csvs()
+    # delete_old_csv()
+    # create_big_csv()
     pass
-    #deleteOldCSV()
-    #cleanCSVs()
-    #createBigCSV()
-    # getCSVs()
-
-
--- data/processdata.py
+++ data/processdata.py
@@ -1,24 +1,19 @@
 import os
 import sys
-
 import lyricsgenius
-import pdb
-import numpy as np
 import csv
 from multiprocessing import Pool
-import time
 import datetime
 
 with open(os.path.join(os.pardir, "credentials"), 'r') as creds:
     client_id = creds.readline()
     client_secret = creds.readline()
     client_access = creds.readlines()
-    creds.close()
 
 # Now we can process the big csv file and add lyrics and stuff
 already_added = []
 infile = "big.csv"
-outfile = "bigout.csv"
+outfile = "bigout2.csv"
 
 
 # Utility function for hiding text output.
@@ -33,139 +28,65 @@ class HiddenPrints:
         sys.stdout = self._original_stdout
 
 
-# #csv file to be read in
-# in_csv = '/path/to/read/file.csv'
-#
-# #csv to write data to
-# out_csv = 'path/to/write/file.csv'
-#
-# #get the number of lines of the csv file to be read
-# number_lines = sum(1 for row in (open(in_csv)))
-#
-# #size of chunks of data to write to the csv
-# chunksize = 10
-#
-# #start looping through data writing it to a new file for each chunk
-# for i in range(1,number_lines,chunksize):
-#      df = pd.read_csv(in_csv,
-#           header=None,
-#           nrows = chunksize,#number of rows to read at each loop
-#           skiprows = i)#skip rows that have been read
-#
-#      df.to_csv(out_csv,
-#           index=False,
-#           header=False,
-#           mode='a',#append data to csv file
-#           chunksize=chunksize)#size of data to append for each loop
 def call_processing_rows_pickably(row):
     return process_row(row)
 
 
 def process_row(row):
-    song = row[1]
-    artist = row[2]
-    if str(song + artist) not in already_added:
-        songLyrics = genius.search_song(song, artist)
-        already_added.append(song + artist)
-        if songLyrics:
-            row.append(str(songLyrics.lyrics).replace("\u2005", " ").replace("\n", " "))
-        else:
-            row.append("Error fetching lyrics")
-    elif str(song + artist) in already_added:
+    song, artist = row[1], row[2]
+    song_artist = ''.join([song, artist])
+
+    if song_artist not in already_added:
+        with HiddenPrints():
+            song_lyrics = genius.search_song(song, artist)
+            already_added.append(song_artist)
+            if song_lyrics:
+                row.append(str(song_lyrics.lyrics).replace("\u2005", " ").replace("\n", " "))
+            else:
+                row.append("Error fetching lyrics")
+    elif song_artist in already_added:
         row.append("duplicate")
 
     return row
 
 
 def append_to_csv(file, list_of_rows):
-    writer = csv.writer(file, lineterminator='\n')
-    writer.writerows(list_of_rows)
+    csv.writer(file, lineterminator='\n').writerows(list_of_rows)
 
 
-class process_csv():
+class ProcessCsv:
 
     def __init__(self, file_name):
-        self.file_name = file_name
-
-    def get_row_count(self):
-        with open(self.file_name, encoding="utf-8") as f:
-            for i, l in enumerate(f):
-                pass
-        self.row_count = i
-
-    def select_chunk_size(self):
         self.chunk_size = 20
-        return
+        self.file_name = file_name
+        with open(self.file_name, encoding="utf-8") as f:
+            self.row_count = sum(1 for _ in f)
 
     def process_rows(self):
         list_de_rows = []
-        count = 0
+
         with open(self.file_name, 'r', encoding='utf-8') as csvIn:
             with open(outfile, "a", encoding='utf-8') as csvOut:
                 reader = csv.reader(csvIn)
-                next(reader)
+                next(reader)  # skip header
                 for row in reader:
-                    # print(count + 1)
                     list_de_rows.append(row)
                     if len(list_de_rows) == self.chunk_size:
+                        i = datetime.datetime.now()
                         append_to_csv(csvOut, p.map(call_processing_rows_pickably, list_de_rows))
+                        f = datetime.datetime.now()
+                        print("per chunk: {0}, per row: {1}".format(f - i, (f - i) / self.chunk_size))
                         del list_de_rows[:]
                 if list_de_rows:
                     append_to_csv(csvOut, p.map(call_processing_rows_pickably, list_de_rows))
 
     def start_process(self):
-        self.get_row_count()
-        self.select_chunk_size()
         self.process_rows()
-
-
-def addlyrics():
-    pass
-    # genius = lyricsgenius.Genius(client_access[0])
-    # genius.remove_section_headers = True
-    # with open(file, 'r') as csvInput:
-
-    # with open("bigout.csv", 'w') as csvOutput:
-    #     writer = csv.writer(csvOutput, lineterminator='\n')
-
-    # reader = csv.reader(csvInput)
-    # all = []
-    # # Header
-    # row = next(reader)
-    # row.append("Lyrics")
-    # all.append(row)
-    # appendLimit = 3  # appends x at a time just in case
-    ##
-    # try:
-    #     try:
-    #         for row in reader:
-    #             song = row[1]
-    #             artist = row[2]
-    #             if str(song + artist) not in already_added:
-    #                 songLyrics = genius.search_song(song, artist)
-    #                 already_added.append(song + artist)
-    #                 # pdb.set_trace()
-    #                 # print(songLyrics.lyrics)
-    #                 # print(already_added)
-    #                 row.append(str(songLyrics.lyrics).replace("\u2005", " ").replace("\n", " "))
-    #             else:
-    #                 row.append("duplicate")
-    #             all.append(row)
-    #             print(len(all) - 1)
-    #     except Exception as e:
-    #         print(e)
-    #         pass
-    #         finally:
-    #             writer.writerows(all)
-    #     except UnicodeEncodeError as e:
-    #         pass
-    #     finally:
-    #         csvOutput.close()
-    # csvInput.close()
 
 
 genius = lyricsgenius.Genius(client_access[0])
 genius.remove_section_headers = True
+
 if __name__ == "__main__":
     with open(infile, 'r', encoding='utf-8') as csvInput:
         with open(outfile, 'w') as csvOutput:
@@ -176,13 +97,11 @@ if __name__ == "__main__":
             row.append("Lyrics")
             writer.writerow(row)
             #
-    with HiddenPrints:
-        initial = datetime.datetime.now()
-        p = Pool()
-        ob = process_csv(infile)
-        ob.start_process()
-        print(ob.row_count)
-        final = datetime.datetime.now()
-    print(final - initial)
 
-    # addlyrics()
+    initial = datetime.datetime.now()
+    p = Pool()
+    ob = ProcessCsv(infile)
+    ob.start_process()
+    print(ob.row_count)
+    final = datetime.datetime.now()
+    print(final - initial)
